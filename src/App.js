fb6c20afb3ab57d9fe76388b49b69d6c3c85c3f1
monishgupta0158 <138368834+monishgupta0158@users.noreply.github.com>
2023-09-08 10:10:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Header from "./components/Header";
+import {Route , Routes} from  'react-router-dom';
+import Blogs from "./pages/Blogs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserBlogs from "./pages/UserBlogs";
+import CreateBlog from "./pages/CreateBlog";
+import BlogDetails from "./pages/BlogDetails";
+
+function App() {
+  return (
+    < >
+    <Header/>
+    <Routes>
+    
+    <Route path="/" element={<Blogs/>}/>
+    <Route path="/blogs" element={<Blogs/>}/>
+   <Route path="/my-blogs" element={<UserBlogs/>}/> 
+   <Route path="/create-blog" element={<CreateBlog />}/> 
+   <Route path="/blog-details/:id" element={<BlogDetails/>}/> 
+    <Route  path="/login" element={<Login/>} />
+    <Route  path="/register" element={<Register/>} />
+    </Routes>
+     
+
+    </>
+  );
+}
+
+export default App;
